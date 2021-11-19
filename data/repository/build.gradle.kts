e1c61a78b2e1d11a0de71206a041e3cdb7650a16
Juan Francisco Mateos <juanfrancisco.mateos@goldenspear.com>
2021-11-19 17:58:40 +0100

    Added Character model

--- data/repository/build.gradle.kts
+++ data/repository/build.gradle.kts
@@ -1,39 +1,13 @@
 plugins {
-    id("com.android.library")
-    id("kotlin-android")
+    id("java-library")
+    id("kotlin")
     id("kotlin-kapt")
 }
 
-android {
-    compileSdk = DefaultConfig.projectCompileSdkVersion
-
-    defaultConfig {
-        minSdk = DefaultConfig.projectMinSdkVersion
-        targetSdk = DefaultConfig.projectTargetSdkVersion
-        testInstrumentationRunner = Androidx.testInstrumentationRunner
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-    kotlinOptions {
-        jvmTarget = JVMTarget.Versions.core
-    }
-    buildTypes {
-        debug { buildConfigField("String", BuildConfig.baseUrl, BuildConfig.baseUrlDebug) }
-        release { buildConfigField("String", BuildConfig.baseUrl, BuildConfig.baseUrlRelease) }
-    }
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
 }
-
 dependencies {
     implementation(project(":domain:model"))
-    implementation(project(":data:repository:model"))
-    implementation(project(":data:open"))
-    implementation("androidx.core:core-ktx:1.7.0")
-    implementation("androidx.appcompat:appcompat:1.3.1")
-    testImplementation("junit:junit:4.13.2")
-    implementation(Google.gson)
-    implementation(SquareApp.retrofit)
-    implementation(SquareApp.retrofitGson)
-
 }
