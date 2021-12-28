41c881354dc9904db64f5cc0545bc38a8badc731
Juan Francisco Mateos <juanfrancisco.mateos@goldenspear.com>
2021-12-28 13:39:14 +0100

    Clean code

--- presentation/ui/src/main/java/com/jfmr/ac/test/presentation/ui/main/RickAndMortyApp.kt
+++ presentation/ui/src/main/java/com/jfmr/ac/test/presentation/ui/main/RickAndMortyApp.kt
@@ -3,7 +3,6 @@ package com.jfmr.ac.test.presentation.ui.main
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.FabPosition
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Surface
@@ -21,45 +20,19 @@ import com.jfmr.ac.test.presentation.ui.navigation.Navigation
 fun RickAndMortyApp() {
     val appState = rememberAppState()
     val navBackStackEntry by appState.navController.currentBackStackEntryAsState()
-    val currentRoute = navBackStackEntry?.destination?.route ?: ""
 
     ThemeAndSurfaceWrapper {
         Scaffold(
             topBar = {
                 MainAppBar()
             },
-            /* floatingActionButton = {
-                 FloatingActionButton(onClick = {}) {
-                     Icon(imageVector = Icons.Default.FavoriteBorder, contentDescription = null)
-                 }
-             },*/
-            floatingActionButtonPosition = FabPosition.Center,
-            isFloatingActionButtonDocked = true,
             bottomBar = {
                 AppBottomNavigation(
-                    currentRoute = currentRoute,
+                    currentRoute = appState.currentRoute,
                     onNavItemClick = {
                         appState.onNavItemClick(it.navCommand.route)
                     }
                 )
-                /* BottomAppBar(cutoutShape = CircleShape) {
-                     AppBarIcon(
-                         imageVector = Icons.Default.Face,
-                         contentDescription = null,
-                         action = {}
-                     )
-                     Spacer(modifier = Modifier.weight(1f))
-                     AppBarIcon(
-                         Icons.Default.KeyboardArrowUp,
-                         contentDescription = stringResource(id = R.string.bottom_bar_left_icon_description),
-                         action = {}
-                     )
-                     AppBarIcon(
-                         Icons.Default.KeyboardArrowDown,
-                         contentDescription = stringResource(id = R.string.bottom_bar_left_icon_description),
-                         action = {}
-                     )
-                 }*/
             },
             scaffoldState = appState.scaffoldState
         ) { padding ->
