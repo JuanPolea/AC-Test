a19ad2acb3811dcdb5f6ae52286a812cdb9aa233
Juan Francisco Mateos <juanfrancisco.mateos@goldenspear.com>
2022-10-11 15:45:21 +0200

    AC-10 #comment Added font, color, theme. Also apply them to Detail and list

--- presentation/ui/src/main/java/com/jfmr/ac/test/presentation/ui/character/detail/view/CharacterDetailScreen.kt
+++ presentation/ui/src/main/java/com/jfmr/ac/test/presentation/ui/character/detail/view/CharacterDetailScreen.kt
@@ -3,9 +3,11 @@ package com.jfmr.ac.test.presentation.ui.character.detail.view
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
@@ -16,6 +18,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
 import androidx.hilt.navigation.compose.hiltViewModel
 import coil.compose.AsyncImage
@@ -73,10 +76,14 @@ internal fun DetailContent(characterDetailState: CharacterDetailState) {
 @Composable
 private fun CharacterDetailSuccessContent(characterDetail: CharacterDetail) {
     Box(
-        modifier = Modifier.fillMaxWidth(),
+        modifier = Modifier
+            .fillMaxWidth(),
         contentAlignment = Alignment.Center
     ) {
-        LazyColumn {
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxWidth()
+        ) {
             item {
                 AsyncImage(
                     model = ImageRequest
@@ -89,28 +96,64 @@ private fun CharacterDetailSuccessContent(characterDetail: CharacterDetail) {
                     contentScale = ContentScale.Crop
                 )
             }
+
             item {
                 Row(
-                    modifier = Modifier.fillMaxWidth(),
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(
+                            top = dimensionResource(id = R.dimen.character_detail_padding),
+                            start = dimensionResource(id = R.dimen.character_detail_padding),
+                            end = dimensionResource(id = R.dimen.character_list_padding),
+                        ),
                 ) {
-                    Text(text = stringResource(id = R.string.name))
-                    Text(text = characterDetail.name)
+                    Text(
+                        text = stringResource(id = R.string.name),
+                        style = MaterialTheme.typography.subtitle1,
+                    )
+                    Text(
+                        text = characterDetail.name,
+                        style = MaterialTheme.typography.h1,
+                    )
                 }
             }
             item {
                 Row(
-                    modifier = Modifier.fillMaxWidth(),
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(
+                            start = dimensionResource(id = R.dimen.character_detail_padding),
+                            end = dimensionResource(id = R.dimen.character_list_padding),
+                        ),
                 ) {
-                    Text(text = stringResource(id = R.string.status))
-                    Text(text = characterDetail.status)
+                    Text(
+                        text = stringResource(id = R.string.status),
+                        style = MaterialTheme.typography.subtitle1,
+                    )
+                    Text(
+                        text = characterDetail.status,
+                        style = MaterialTheme.typography.h1,
+                    )
                 }
             }
             item {
                 Row(
-                    modifier = Modifier.fillMaxWidth(),
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(
+                            top = dimensionResource(id = R.dimen.character_detail_padding),
+                            start = dimensionResource(id = R.dimen.character_detail_padding),
+                            end = dimensionResource(id = R.dimen.character_list_padding),
+                        ),
                 ) {
-                    Text(text = stringResource(id = R.string.location))
-                    Text(text = characterDetail.location?.name ?: "")
+                    Text(
+                        text = stringResource(id = R.string.location),
+                        style = MaterialTheme.typography.subtitle1,
+                    )
+                    Text(
+                        text = characterDetail.location?.name ?: "",
+                        style = MaterialTheme.typography.h1,
+                    )
                 }
             }
         }
--- presentation/ui/src/main/java/com/jfmr/ac/test/presentation/ui/main/theme/Theme.kt
+++ presentation/ui/src/main/java/com/jfmr/ac/test/presentation/ui/main/theme/Theme.kt
@@ -2,40 +2,19 @@ package com.jfmr.ac.test.presentation.ui.main.theme
 
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.darkColors
-import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 
-private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
-)
-
-private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200
-
-    /* Other default colors to override
-    background = Color.White,
-    surface = Color.White,
-    onPrimary = Color.White,
-    onSecondary = Color.Black,
-    onBackground = Color.Black,
-    onSurface = Color.Black,
-    */
-)
 
 @Composable
 fun ACTestTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable() () -> Unit) {
     val colors = if (darkTheme) {
-        DarkColorPalette
+        DarkColors
     } else {
-        LightColorPalette
+        LightColors
     }
     MaterialTheme(
         colors = colors,
         content = content,
+        typography = RAMTypography
     )
 }
