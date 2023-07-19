2d77683a41fa46b319de94bb5bf531c2b2938290
Juan Francisco Mateos <juanfrancisco.mateos@goldenspear.com>
2023-07-19 20:02:01 +0200

    AC #comment Update dependencies

--- presentation/ui/src/main/java/com/jfmr/ac/test/presentation/ui/main/theme/Color.kt
+++ presentation/ui/src/main/java/com/jfmr/ac/test/presentation/ui/main/theme/Color.kt
@@ -4,19 +4,25 @@ import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.ui.graphics.Color
 
-private val primaryColor = Color(0xFF00d25a)
-private val secondaryColor = Color(0xFFEBE540)
-private val background = Color(0xFFE4F9E8)
+private val md_theme_light_primary = Color(0xFF00d25a)
+private val md_theme_light_onPrimary = Color(0xFFEBE540)
+private val md_theme_light_primaryContainer = Color(0xFFE4F9E8)
+private val md_theme_light_onPrimaryContainer = Color(0xFFA9F5B7)
 
-private val primaryDarkColor = Color(0xFFdd00ca)
-private val secondaryDarkColor = Color(0xFFed8be0)
+private val md_theme_dark_onPrimary = Color(0xFFed8be0)
+private val md_theme_dark_primary = Color(0xFFdd00ca)
+private val md_theme_dark_primaryContainer = Color(0xFF313131)
+private val md_theme_dark_onPrimaryContainer = Color(0xFFDBC8C8)
 
-val LightColors = lightColorScheme(
-    primary = primaryColor,
-    secondary = secondaryColor,
-    background = background
+val LightColorScheme = lightColorScheme(
+    primary = md_theme_light_primary,
+    onPrimary = md_theme_light_onPrimary,
+    primaryContainer = md_theme_light_primaryContainer,
+    onPrimaryContainer = md_theme_light_onPrimaryContainer,
 )
-val DarkColors = darkColorScheme(
-    primary = primaryDarkColor,
-    secondary = secondaryDarkColor,
+val DarkColorScheme = darkColorScheme(
+    primary = md_theme_dark_primary,
+    onPrimary = md_theme_dark_onPrimary,
+    primaryContainer = md_theme_dark_primaryContainer,
+    onPrimaryContainer = md_theme_dark_onPrimaryContainer,
 )
--- presentation/ui/src/main/java/com/jfmr/ac/test/presentation/ui/main/theme/Theme.kt
+++ presentation/ui/src/main/java/com/jfmr/ac/test/presentation/ui/main/theme/Theme.kt
@@ -1,16 +1,22 @@
 package com.jfmr.ac.test.presentation.ui.main.theme
 
+import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.dynamicDarkColorScheme
+import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
 
 
 @Composable
 fun ACTestTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
-    val colors = if (darkTheme) {
-        DarkColors
-    } else {
-        LightColors
+    val dynamicColor = Build.VERSION.SDK_INT >= Build.VERSION_CODES.S
+    val colors = when {
+        dynamicColor && darkTheme -> dynamicDarkColorScheme(LocalContext.current)
+        dynamicColor && !darkTheme -> dynamicLightColorScheme(LocalContext.current)
+        darkTheme -> DarkColorScheme
+        else -> LightColorScheme
     }
     MaterialTheme(
         colorScheme = colors,
